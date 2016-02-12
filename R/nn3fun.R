688d89a1f0a5ef1c04f4203132ba7e9c63b6380d
Luis-Sanguiao <luis.sanguiao.sande@gmail.com>
2016-02-12 13:15:53 +0100

    Code donor=2 now means both donor and receiver

--- R/nn3fun.R
+++ R/nn3fun.R
@@ -5,14 +5,14 @@ nn3fun<-function(id,stratum,values,donor,nvar) {
   cinv<-chol2inv(chol(var(t(matrix(values,nrow=nvar)),use="na.or.complete")))
 
   # We create receivers matrix and vectors for unit and stratum identification
-  matreceivers<-matrix(values[!donor],nrow=nvar)
-  idreceivers<-id[!donor][seq.int(1,ncol(matreceivers),nvar)]
-  strreceivers<-stratum[!donor][seq.int(1,ncol(matreceivers),nvar)]
+  matreceivers<-matrix(values[donor!=1],nrow=nvar)
+  idreceivers<-id[donor!=1][seq.int(1,ncol(matreceivers),nvar)]
+  strreceivers<-stratum[donor!=1][seq.int(1,ncol(matreceivers),nvar)]
 
   # Same for donors
-  matdonors<-matrix(values[donor],nrow=nvar)
-  iddonors<-id[donor][seq.int(1,ncol(matdonors),nvar)]
-  strdonors<-stratum[donor][seq.int(1,ncol(matdonors),nvar)]
+  matdonors<-matrix(values[donor!=0],nrow=nvar)
+  iddonors<-id[donor!=0][seq.int(1,ncol(matdonors),nvar)]
+  strdonors<-stratum[donor!=0][seq.int(1,ncol(matdonors),nvar)]
 
   # Mahalanobis distance
   distance<-function(x) t(x)%*%cinv%*%x
@@ -22,8 +22,12 @@ nn3fun<-function(id,stratum,values,donor,nvar) {
   countneighbours<-numeric(length(idreceivers))
   distneighbours<-numeric(length(idreceivers))
   for(str in unique(stratum)) {
-    distmat<-apply(matreceivers[,strreceivers==str,drop=FALSE],2,function(x)
-      apply(matdonors[,strdonors==str,drop=FALSE]-x,2,distance))
+    distmat<-apply(rbind(1:length(idreceivers),matreceivers)[,strreceivers==str,drop=FALSE],2,function(x) {
+      temp<-matdonors[,iddonors==idreceivers[x[1]]]
+      matdonors[,iddonors==idreceivers[x[1]]]<-NA
+      output<-apply(matdonors[,strdonors==str,drop=FALSE]-x[-1],2,distance)
+      matdonors[,iddonors==idreceivers[x[1]]]<-temp
+      return(output)})
     if(is.null(dim(distmat))) distmat<-matrix(distmat,nrow=1)
     selecteddonors<-apply(distmat,2,which.min)
     if (length(selecteddonors)) {
@@ -45,9 +49,9 @@ nn3fun<-function(id,stratum,values,donor,nvar) {
   output.id<-rep(NA,length=length(id))
   output.count<-rep(NA,length=length(id))
   output.dist<-rep(NA,length=length(id))
-  output.id[!donor]<-rep(idneighbours,each=nvar)
-  output.count[!donor]<-rep(countneighbours,each=nvar)
-  output.dist[!donor]<-rep(distneighbours,each=nvar)
+  output.id[donor!=1]<-rep(idneighbours,each=nvar)
+  output.count[donor!=1]<-rep(countneighbours,each=nvar)
+  output.dist[donor!=1]<-rep(distneighbours,each=nvar)
   return(list(output.id,output.count,output.dist))
 
 }
