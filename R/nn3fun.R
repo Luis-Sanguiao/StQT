3f8caeef4ded531658a796e46304b687e89492b0
Luis-Sanguiao <luis.sanguiao.sande@gmail.com>
2016-02-12 10:02:14 +0100

    Now nn3fun returns distances

--- R/nn3fun.R
+++ R/nn3fun.R
@@ -20,24 +20,34 @@ nn3fun<-function(id,stratum,values,donor,nvar) {
   # Computation of the nearest neighbour for each receiver by stratum
   idneighbours<-idreceivers
   countneighbours<-numeric(length(idreceivers))
+  distneighbours<-numeric(length(idreceivers))
   for(str in unique(stratum)) {
     distmat<-apply(matreceivers[,strreceivers==str,drop=FALSE],2,function(x)
       apply(matdonors[,strdonors==str,drop=FALSE]-x,2,distance))
     if(is.null(dim(distmat))) distmat<-matrix(distmat,nrow=1)
     selecteddonors<-apply(distmat,2,which.min)
-    selecteddistances<-distmat[(0:(ncol(distmat)-1))*nrow(distmat)+selecteddonors]
-    orderedreceivers<-order(selecteddonors,selecteddistances)
-    ordereddonors<-selecteddonors[orderedreceivers]
-    auxvector<-ordereddonors-c(ordereddonors[1],ordereddonors)[-length(ordereddonors)-1]
-    countneighbours[strreceivers==str][orderedreceivers]<-
-      (1:length(auxvector))-cummax(as.numeric(auxvector!=0)*(0:(length(auxvector)-1)))
-    if (length(selecteddonors)) idneighbours[strreceivers==str]<-iddonors[strdonors==str][selecteddonors]
-    else idneighbours[strreceivers==str]<-NA
+    if (length(selecteddonors)) {
+        selecteddistances<-distmat[(0:(ncol(distmat)-1))*nrow(distmat)+selecteddonors]
+        orderedreceivers<-order(selecteddonors,selecteddistances)
+        ordereddonors<-selecteddonors[orderedreceivers]
+        auxvector<-ordereddonors-c(ordereddonors[1],ordereddonors)[-length(ordereddonors)-1]
+        countneighbours[strreceivers==str][orderedreceivers]<-
+          (1:length(auxvector))-cummax(as.numeric(auxvector!=0)*(0:(length(auxvector)-1)))
+        distneighbours[strreceivers==str]<-selecteddistances
+        idneighbours[strreceivers==str]<-iddonors[strdonors==str][selecteddonors]
+    }
+    else {
+        idneighbours[strreceivers==str]<-NA
+        countneighbours[strreceivers==str]<-NA
+        distneighbours[strreceivers==str]<-NA
+    }
   }
   output.id<-rep(NA,length=length(id))
   output.count<-rep(NA,length=length(id))
+  output.dist<-rep(NA,length=length(id))
   output.id[!donor]<-rep(idneighbours,each=nvar)
   output.count[!donor]<-rep(countneighbours,each=nvar)
-  return(list(output.id,output.count))
+  output.dist[!donor]<-rep(distneighbours,each=nvar)
+  return(list(output.id,output.count,output.dist))
 
 }
