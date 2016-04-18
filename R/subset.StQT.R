9cf3353279b333cf5adaeaba2293b18ad614976c
Luis-Sanguiao <luis.sanguiao.sande@gmail.com>
2016-04-18 13:31:26 +0200

    Help files updated: subset

--- R/subset.StQT.R
+++ R/subset.StQT.R
@@ -1,22 +1,31 @@
-#' Extrae partes de un objeto de clase StQT.
+#' Extracts parts of an StQT object.
 #'
-#' \code{[} extrae o reemplaza partes de un objeto de clase \linkS4class{StQT}.
+#' \code{[} extracts parts of an object of class \linkS4class{StQT}.
 #'
-#' Se trata del método \code{[} para la clase \linkS4class{StQT}. Este método
-#' obtiene subconjuntos del objeto de clase \code{StQT} especificado como entrada.
+#' It is indeed the method \code{[} for the class \linkS4class{StQT}. This method
+#' returns an object of class \linkS4class{StQT} whose rules are the specified subset
+#' of the original rules.
 #'
-#' @param x objeto de clase \linkS4class{StQT} del que se van a extraer los
-#' elementos.
+#' @param x object of class \linkS4class{StQT} from which to extract element(s).
 #'
-#' @param i índices correspondientes a los elementos a extraer.
+#' @param i indices specifying elements to extract.
 #'
-#' @param drop Incluido por coherencia.
+#' @param drop Included by coherence.
 #'
-#' @return Objeto de clase \linkS4class{StQT}, que consiste en un subconjunto del
-#'  objeto \code{StQT} de entrada.
+#' @return Object of class \linkS4class{StQT}, with the subset of rules.
 #'
 #' @examples
-#'
+#' rules <- data.frame(domain = c('!is.na(Variable)','is.na(Variable)',''),
+#'                     output = c('MediaVariable','Variable','MediaVariable'),
+#'                     fun =    c('mean','identity','FunDelVar'),
+#'                     input =  c('Variable','MediaVariable',''),
+#'                     by =     c('Estrato','',''),
+#'                     key =    c('Estrato','',''),
+#'                     stringsAsFactors = FALSE)
+#' imputa_media <- NewStQT(rules)
+#' imputa_media
+#' imputa_media[1:2]
+#' imputa_media[3:1]
 #'
 #' @include StQT-class.R
 #'
