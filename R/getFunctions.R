2ee6ae5620c3058f675198db03f1882fc82bb8e5
Luis-Sanguiao <luis.sanguiao.sande@gmail.com>
2016-04-19 09:40:11 +0200

    Help files updated: getRules and getFunctions

--- R/getFunctions.R
+++ R/getFunctions.R
@@ -1,15 +1,22 @@
-#' Devuelve el slot Functions de un objeto de clase StQT.
+#' Returns slot \code{Functions} from an object of class \linkS4class{StQT}
 #'
-#' \code{getFunctions} devuelve el slot \code{Functions} del input
-#' \code{\linkS4class{StQT}}.
+#' \code{getFunctions} extracts slot \code{Functions} from an \code{\linkS4class{StQT}} object.
 #'
-#' @param object Objeto de clase \code{\linkS4class{StQT}} del que se desea
-#' extraer las funciones.
+#' @param object Object of class \code{\linkS4class{StQT}}
 #'
-#' @return Una \code{\link{list}} con las funciones de transformación.
+#' @return A \code{\link{list}} corresponding to the slot \code{Functions} of the input parameter.
 #'
 #' @examples
-#'
+#' rules <- data.frame(domain = c('!is.na(Variable)','is.na(Variable)',''),
+#'                     output = c('MediaVariable','Variable','MediaVariable'),
+#'                     fun =    c('mean','identity','FunDelVar'),
+#'                     input =  c('Variable','MediaVariable',''),
+#'                     by =     c('Estrato','',''),
+#'                     key =    c('Estrato','',''),
+#'                     stringsAsFactors = FALSE)
+#' T1 <- NewStQT(rules)
+#' T1
+#' getFunctions(T1)
 #'
 #' @include StQT-class.R
 #'
--- R/getRules.R
+++ R/getRules.R
@@ -1,14 +1,20 @@
-#' Devuelve el slot Rules de un objeto de clase StQT.
+#' Returns slot \code{Rules} from an object of class \linkS4class{StQT}
 #'
-#' \code{getRules} devuelve el slot \code{Rules} del input
-#' \code{\linkS4class{StQT}}.
+#' \code{getRules} extracts slot \code{Rules} from an \code{\linkS4class{StQT}} object.
 #'
-#' @param object Objeto de clase \code{\linkS4class{StQT}} del que se desea
-#' extraer las reglas.
+#' @param object Object of class \code{\linkS4class{StQT}}
 #'
-#' @return Un \code{\link{data.frame}} con las reglas de la transformación.
+#' @return A \code{\link{data.frame}} corresponding to the slot \code{Rules} of the input parameter.
 #'
 #' @examples
+#' T1 <- NewStQT(data.frame(
+#'                 domain = c("Next==Unit","","Value < 0"),
+#'                 output = c("Value_next","Value_next",""),
+#'                    fun = c("FunAutoLink","FunDelVar","FunDelRow"),
+#'                  input = c("Value","",""),
+#'                          stringsAsFactors = FALSE))
+#' T1
+#' getRules(T1)
 #'
 #'
 #' @include StQT-class.R
